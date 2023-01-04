e767b88455e1835f35bd0afa41e5259173fec722
mfurmanczak <mfurmanczak@gmail.com>
2023-01-04 13:56:44 +0100

    added secret key

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("hello world") 
+from calendar_api.calendar_api import google_calendar_api
+m=google_calendar_api()
+m.create_event(calendar_id='AIzaSyCSGlDRqLrv8mHlih_431aVIw6SQ4knFf4',
+    start='2017,12,5,15,00,00',
+    end='2017,12,5,15,15,00'
+    description='foo'
+)
