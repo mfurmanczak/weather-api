cf9f4f697e0a70a966117559393e39ca53962502
mfurmanczak <mfurmanczak@gmail.com>
2023-01-04 18:28:09 +0100

    git

--- displayWeather.py
+++ displayWeather.py
@@ -1,5 +1,28 @@
-import metoffer
+# import the module
+import python_weather
+import asyncio
+import os
 
-api_key = '01234567-89ab-cdef-0123-456789abcdef'
-M = metoffer.MetOffer(api_key)
-x = M.nearest_loc_forecast(54.972656, -1.614074, MetOffer.THREE_HOURLY)
+async def getweather():
+  # declare the client. format defaults to the metric system (celcius, km/h, etc.)
+  async with python_weather.Client(format=python_weather.METRIC) as client:
+
+    # fetch a weather forecast from a city
+    weather = await client.get("Newcastle upon Tyne")
+  
+    # returns the current day's forecast temperature (int)
+    print(weather.current.temperature)
+  
+    # get the weather forecast for a few days
+    for forecast in weather.forecasts:
+      print(forecast.date, forecast.astronomy)
+  
+      # hourly forecasts
+      for hourly in forecast.hourly:
+        print(f' --> {hourly!r}')
+
+if __name__ == "__main__":
+  if os.name == "nt":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+  asyncio.run(getweather())
